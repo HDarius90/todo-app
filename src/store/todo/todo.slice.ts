4580fb09f658197524d0b9c34aa87b0931a6a842
Hadjati Darius <72970048+HDarius90@users.noreply.github.com>
2025-03-03 22:24:55 +0100

    add uniq ids for todos

--- src/store/todo/todo.slice.ts
+++ src/store/todo/todo.slice.ts
@@ -1,23 +1,9 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { TodoState } from './todo.types';
+import { v4 as uuidv4 } from 'uuid';
 
 const initialState: TodoState = {
-  todos: [
-    { id: 1, text: 'Take out the trash', completed: false },
-    { id: 2, text: 'Go for a walk', completed: false },
-    { id: 3, text: 'VERRY LOOOOOOOOOOOOOOOONG TASK', completed: true },
-    { id: 4, text: 'Push commits to develope', completed: true },
-    { id: 5, text: 'Push commits to develope', completed: true },
-    { id: 6, text: 'Push commits to develope', completed: true },
-    { id: 7, text: 'Push commits to develope', completed: true },
-    { id: 8, text: 'Push commits to develope', completed: true },
-    { id: 9, text: 'Push commits to develope', completed: true },
-    { id: 10, text: 'Push commits to develope', completed: true },
-    { id: 11, text: 'Push commits to develope', completed: true },
-    { id: 12, text: 'Push commits to develope', completed: true },
-    { id: 13, text: 'Push commits to develope', completed: true },
-    { id: 14, text: 'Push commits to develope', completed: true },
-  ],
+  todos: [],
 };
 
 const todoSlice = createSlice({
@@ -26,16 +12,16 @@ const todoSlice = createSlice({
   reducers: {
     addTodo: (state, action: PayloadAction<string>) => {
       state.todos.push({
-        id: Date.now(),
+        id: uuidv4(),
         text: action.payload,
         completed: false,
       });
     },
-    toggleTodo: (state, action: PayloadAction<number>) => {
+    toggleTodo: (state, action: PayloadAction<string>) => {
       const todo = state.todos.find((todo) => todo.id === action.payload);
       if (todo) todo.completed = !todo.completed;
     },
-    removeTodo: (state, action: PayloadAction<number>) => {
+    removeTodo: (state, action: PayloadAction<string>) => {
       state.todos = state.todos.filter((todo) => todo.id !== action.payload);
     },
     removeAllTodo: (state) => {
