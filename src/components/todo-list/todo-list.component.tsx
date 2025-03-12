62c667e2b30a8e065ce28fb4319abffddda5114b
Hadjati Darius <72970048+HDarius90@users.noreply.github.com>
2025-03-12 20:30:00 +0100

    add task toggle handler

--- src/components/todo-list/todo-list.component.tsx
+++ src/components/todo-list/todo-list.component.tsx
@@ -14,19 +14,48 @@ const TodoList = () => {
   const dispatch = useDispatch();
   const todos = useSelector((state: RootState) => state.todos.allTodos);
 
+  const handleToggle = (id: string, event: React.MouseEvent<HTMLLIElement>) => {
+    // If the event target is the checkbox, ignore the click event
+    if (
+      event.target instanceof HTMLInputElement &&
+      event.target.type === 'checkbox'
+    ) {
+      return;
+    }
+
+    event.stopPropagation();
+    dispatch(toggleTodo(id));
+  };
+
+  const handleCheckboxChange = (id: string) => {
+    dispatch(toggleTodo(id));
+  };
+
+  const handleRemove = (
+    id: string,
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    event.stopPropagation();
+    dispatch(removeTodo(id));
+  };
+
   return (
     <List>
       {todos.map((todo) => (
         <TodoItem
           key={todo.id}
           completed={todo.completed}
-          onClick={() => dispatch(toggleTodo(todo.id))}
+          onClick={(e) => handleToggle(todo.id, e)}
         >
-          <Checkbox type="checkbox" checked={todo.completed} />
+          <Checkbox
+            type="checkbox"
+            checked={todo.completed}
+            onChange={() => handleCheckboxChange(todo.id)}
+          />
           <TodoContent>{todo.text}</TodoContent>
           <Button
             buttonType={BUTTON_TYPE_CLASSES.delete}
-            onClick={() => dispatch(removeTodo(todo.id))}
+            onClick={(e) => handleRemove(todo.id, e)}
           >
             <TrashIcon />
           </Button>
